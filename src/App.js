c10dfc64566849c67dd7fa9ae1e6f54c00cf8053
luis <luisarthe27@gmail.com>
2020-07-13 16:59:52 -0500

    restructure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
